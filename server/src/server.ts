9f86651873f27e2247b0b995c8d0689f5c702587
Patrick Furtak <patrick@patrickfurtak.com>
2020-12-14 13:59:55 -0500

    Installed dev dependencies and typescript type defs, typescript compiler options, restructured file directory

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT: number = 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `[server.ts]: Express server listening on http://localhost:${PORT}...`
+  );
+});
