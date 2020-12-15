bff05955c66ebd505a1efda49991ab0873bf98b2
Patrick Furtak <patrick@patrickfurtak.com>
2020-12-15 14:25:36 -0500

    Installed gql dependencies, wrote test query and mutation endpoints

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { listings } from './listings';
+import { ApolloServer } from 'apollo-server-express';
+import { schema } from './graphql';
 
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
-
-// GET listings
-app.get('/listings', (_req, res) => {
-  return res.send(listings);
-});
-
-// DELETE listing
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-  }
-  return res.send('Listing not found');
-});
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(PORT, () => {
   console.log(
